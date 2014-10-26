ac825b4bc388c96860c130b7774a5baea6ad9873
daniel <paundanielionut@gmail.com>
2014-10-26 22:00:11 +0200

    commit lab2

---
+++ lab2/src/pkg/lab2classMAIN.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pkg;
+
+/**
+ * @author Daniel
+ *
+ */
+public class lab2classMAIN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
